965cbbfeefc348e272ddb49c794ab1f6a3de9a5e
André Veríssimo <afsverissimo@gmail.com>
2021-06-03 14:57:37 +0200

    first release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='ffmpeg_transcompile',
-  version='0.1',
+  version='1.0',
   description='A plugin to transcompile videos',
   url='http://github.com/averissimo/ffmpeg_transcompile',
   author='André Veríssimo',
